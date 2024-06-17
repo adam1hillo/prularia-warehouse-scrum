12eca5ab0815058dbb4a5803c1e7f684b4fcc702
Hans Desmet <hans.desmet@vdabcampus.be>
2024-06-17 10:15:35 +0200

    Initial Commit

---
+++ src/main/java/be/vdab/scrumjava202406/ScrumJava202406Application.java
@@ -0,0 +1,13 @@
+package be.vdab.scrumjava202406;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumJava202406Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrumJava202406Application.class, args);
+    }
+
+}
---
+++ src/test/java/be/vdab/scrumjava202406/ScrumJava202406ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.scrumjava202406;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumJava202406ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
