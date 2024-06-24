9167f76a04e1d7624dd4917b3f4612a0228603a0
businan <yorulmazburaksinan@gmail.com>
2024-06-24 14:32:22 +0200

    artikeldetails met plaatsen gemaakt (DTO)

--- src/main/java/be/vdab/scrumjava202406/bestellingen/ArtikelController.java
+++ src/main/java/be/vdab/scrumjava202406/bestellingen/ArtikelController.java
@@ -5,18 +5,39 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Stream;
+
 @RestController
 @RequestMapping("artikels")
 public class ArtikelController {
     private final ArtikelService artikelService;
+    private static MagazijnPlaatsService magazijnPlaatsService = null;
 
-    public ArtikelController(ArtikelService artikelService) {
+    public ArtikelController(ArtikelService artikelService,MagazijnPlaatsService magazijnPlaatsService) {
         this.artikelService = artikelService;
+        this.magazijnPlaatsService = magazijnPlaatsService;
     }
 
-    @GetMapping("{id}")
+    private record ArtikelMetPlaatsenDTO(long artikelId, String naam, BigDecimal prijs, long gewichtInGram, int voorraad, int maximumVoorraad, int maxAantalInMagazijnPlaats, List<MagazijnPlaats> magazijnPlaatsen) {
+        ArtikelMetPlaatsenDTO(Artikel artikel){
+            this(artikel.getArtikelId(), artikel.getNaam(), artikel.getPrijs(), artikel.getGewichtInGram(),
+                    artikel.getVoorraad(), artikel.getMaximumVoorraad(), artikel.getMaxAantalInMagazijnPlaats(),  magazijnPlaatsService.findAllPlaatsById(artikel.getArtikelId())
+                    );
+        }
+    }
+
+    /*@GetMapping("{id}")
     Artikel findById(@PathVariable long id) {
         return artikelService.findById(id)
+                .orElseThrow(() -> new ArtikelNietGevondenException(id));
+    }*/
+
+    @GetMapping("{id}")
+    ArtikelMetPlaatsenDTO findById(@PathVariable long id) {
+        return artikelService.findById(id)
+                .map(ArtikelMetPlaatsenDTO::new)
                 .orElseThrow(() -> new ArtikelNietGevondenException(id));
     }
 }
