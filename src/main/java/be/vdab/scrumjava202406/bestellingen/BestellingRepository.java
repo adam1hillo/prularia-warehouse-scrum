4b76c285ce1d82ec66845fe21994aee8b7d1d4c5
Hortix <hortiix@gmail.com>
2024-06-24 12:07:38 +0200

    .3 add countBestellingKlaarOmGepickt in BestellingRepository

---
+++ src/main/java/be/vdab/scrumjava202406/bestellingen/BestellingRepository.java
@@ -0,0 +1,22 @@
+package be.vdab.scrumjava202406.bestellingen;
+
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BestellingRepository {
+    private static final int BETAALD_STATUS_ID = 2;
+    private final JdbcClient jdbcClient;
+
+    public BestellingRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+
+    long countBestellingKlaarOmGepickt() {
+        var sql = """
+                SELECT COUNT(Bestellingen.bestelId) as bestelingTotal FROM Bestellingen
+                WHERE bestellingsStatusId = ?;
+                """;
+        return (long) jdbcClient.sql(sql).param(BETAALD_STATUS_ID).query().singleValue();
+    }
+}
