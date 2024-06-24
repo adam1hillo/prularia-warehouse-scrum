4dd5a593b06eec50535948ddae7e75fe89bafbfa
businan <yorulmazburaksinan@gmail.com>
2024-06-24 14:11:49 +0200

    MagazijnPlaats domein tot controller gemaakt

--- src/main/java/be/vdab/scrumjava202406/bestellingen/MagazijnPlaats.java
+++ src/main/java/be/vdab/scrumjava202406/bestellingen/MagazijnPlaats.java
@@ -1,4 +1,26 @@
 package be.vdab.scrumjava202406.bestellingen;
 
 public class MagazijnPlaats {
+    private final char rij;
+    private final int rek;
+    private final int aantal;
+
+    public MagazijnPlaats(char rij, int rek, int aantal) {
+
+        this.rij = rij;
+        this.rek = rek;
+        this.aantal = aantal;
+    }
+
+    public char getRij() {
+        return rij;
+    }
+
+    public int getRek() {
+        return rek;
+    }
+
+    public int getAantal() {
+        return aantal;
+    }
 }
---
+++ src/main/java/be/vdab/scrumjava202406/bestellingen/MagazijnPlaatsController.java
@@ -0,0 +1,23 @@
+package be.vdab.scrumjava202406.bestellingen;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("magazijnPlaatsen")
+public class MagazijnPlaatsController {
+    private final MagazijnPlaatsService magazijnPlaatsService;
+
+    public MagazijnPlaatsController(MagazijnPlaatsService magazijnPlaatsService) {
+        this.magazijnPlaatsService = magazijnPlaatsService;
+    }
+
+    @GetMapping("{id}")
+    List<MagazijnPlaats> findAllPlaatsById(@PathVariable long id) {
+        return magazijnPlaatsService.findAllPlaatsById(id);
+    }
+}
