04e58823918a042b44d21a6295139f2030934782
MarcoFacchin <marco.facchin@vdabcampus.be>
2024-06-24 14:53:36 +0200

    testscripts verbeterd

--- src/test/resources/artikelen.sql
+++ src/test/resources/artikelen.sql
@@ -1,6 +1,6 @@
-insert into artikelen(naam, gewichtInGram) values
-('testartikel1', 1),
-('testartikel2', 2),
-('testartikel3', 3),
-('testartikel4', 4),
-('testartikel5', 5);
+insert into artikelen(naam, gewichtInGram, prijs, beschrijving, ean, leveranciersId) values
+('testartikel1', 1, 10.0, 'test', 'test', 1),
+('testartikel2', 2, 10.0, 'test', 'test', 1),
+('testartikel3', 3, 10.0, 'test', 'test', 1),
+('testartikel4', 4, 10.0, 'test', 'test', 1),
+('testartikel5', 5, 10.0, 'test', 'test', 1);
--- src/test/resources/bestellingen.sql
+++ src/test/resources/bestellingen.sql
@@ -1,6 +1,6 @@
-insert into bestellingen(besteldatum, bedrijfsnaam) values
-('1900-01-01 00:00:00', 'testbedrijf1'),
-('1900-01-01 00:00:00', 'testbedrijf2'),
-('1900-01-01 00:00:00', 'testbedrijf3'),
-('1900-01-01 00:00:00', 'testbedrijf4'),
-('1900-01-01 00:00:00', 'testbedrijf5');
+insert into bestellingen(besteldatum, bedrijfsnaam, betaalwijzeId, klantId, leveringsAdresId, voornaam, familienaam, bestellingsStatusId, facturatieAdresId) values
+('1900-01-01 00:00:00', 'testbedrijf1', 1, 1, 1, 'test', 'test', 1, 1),
+('1900-01-01 00:00:00', 'testbedrijf2', 1, 1, 1, 'test', 'test', 1, 1),
+('1900-01-01 00:00:00', 'testbedrijf3', 1, 1, 1, 'test', 'test', 1, 1),
+('1900-01-01 00:00:00', 'testbedrijf4', 1, 1, 1, 'test', 'test', 1, 1),
+('1900-01-01 00:00:00', 'testbedrijf5', 1, 1, 1, 'test', 'test', 1, 1);
