16f0f90e397fd18a4633d18e76a5820e43b1ea3e
businan <yorulmazburaksinan@gmail.com>
2024-06-24 13:13:14 +0200

    artikelRepo voor demo

---
+++ src/main/java/be/vdab/scrumjava202406/bestellingen/ArtikelRepository.java
@@ -0,0 +1,25 @@
+package be.vdab.scrumjava202406.bestellingen;
+
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class ArtikelRepository {
+    private final JdbcClient jdbcClient;
+
+    public ArtikelRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+
+    // sergio scrijf maar jouw method hier
+    Optional<Artikel> findById(long id) {
+        String sql = """
+               """;
+        return jdbcClient.sql(sql)
+                .param(id)
+                .query(Artikel.class)
+                .optional();
+    }
+}
