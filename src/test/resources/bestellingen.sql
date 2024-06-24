ff2da7ca67ce1e27b17f21d3b907d5aee6f58ac3
nico <nicoverrijdt@gmail.com>
2024-06-24 15:01:48 +0200

    De sql mock data voor de test

---
+++ src/test/resources/bestellingen.sql
@@ -0,0 +1,5 @@
+insert into Artikelen(ean, naam, beschrijving, prijs, gewichInGram, bestelpeil,
+                      voorraad, minimumVoorraad, maximumVoorraad, levertijd,
+                      aantalBesteldeLeverancier, maxAantalInMagazijnPlaats, leveranciersId) values
+    ('0', 'test1', '0', 1.0, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+    ('1', 'test2', '1', 1.0, 1, 1, 1, 1, 1, 1, 1, 1, 1);
