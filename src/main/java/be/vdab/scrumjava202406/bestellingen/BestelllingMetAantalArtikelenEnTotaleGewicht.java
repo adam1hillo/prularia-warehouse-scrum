e8be4bebd45c7788fdbf7046cfc517e0466bdc70
Adam Hillo <adam1hillo@gmail.com>
2024-06-24 13:14:11 +0200

    DTO voor oudste 5 bestellingen (id, aantalArtikelen, totaleGewicht) (visibility aangepast)

--- src/main/java/be/vdab/scrumjava202406/bestellingen/BestelllingMetAantalArtikelenEnTotaleGewicht.java
+++ src/main/java/be/vdab/scrumjava202406/bestellingen/BestelllingMetAantalArtikelenEnTotaleGewicht.java
@@ -2,5 +2,5 @@ package be.vdab.scrumjava202406.bestellingen;
 
 import java.math.BigDecimal;
 
-public record BestelllingMetAantalArtikelenEnTotaleGewicht(long id, int aantalArtikelen, BigDecimal totaleGewicht) {
+record BestelllingMetAantalArtikelenEnTotaleGewicht(long id, int aantalArtikelen, BigDecimal totaleGewicht) {
 }
