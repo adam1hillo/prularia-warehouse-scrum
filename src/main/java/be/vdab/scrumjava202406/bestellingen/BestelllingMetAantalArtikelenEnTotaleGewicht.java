0863414d151cf88a327dfd10769aca6ea3a991f7
Hortix <hortiix@gmail.com>
2024-06-24 15:31:36 +0200

    refactor BestelllingMetAantalArtikelenEnTotaleGewicht name tot BestellingOverzicht

--- src/main/java/be/vdab/scrumjava202406/bestellingen/BestelllingMetAantalArtikelenEnTotaleGewicht.java
+++ src/main/java/be/vdab/scrumjava202406/bestellingen/BestelllingMetAantalArtikelenEnTotaleGewicht.java
@@ -2,5 +2,5 @@ package be.vdab.scrumjava202406.bestellingen;
 
 import java.math.BigDecimal;
 
-record BestelllingMetAantalArtikelenEnTotaleGewicht(long id, int aantalArtikelen, BigDecimal totaleGewicht) {
+record BestellingOverzicht(long id, int aantalArtikelen, BigDecimal totaleGewicht) {
 }
