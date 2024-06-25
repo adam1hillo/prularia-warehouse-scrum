ff10881005bdd8c35a11bbd376972b1352203fc2
Gilian Hoskens <gilianhoskens@gmail.com>
2024-06-25 15:40:39 +0200

    Code Review > Data wordt nu gefetched uit database

--- src/main/resources/static/js/tvscherm.js
+++ src/main/resources/static/js/tvscherm.js
@@ -1,8 +1,8 @@
 "use strict";
 import {setText, toon} from "./util.js";
-//const response = await fetch("bestellingen/aantal");
-const response = await fetch("http://localhost:63342/ScrumJava202406/src/main/resources/static/js/aantalbestellingen.json");
-console.log(response);
+
+const response = await fetch("bestellingen/aantal");
+
 if (response.ok) {
     const body = await response.text();
     setText("aantal", body);
