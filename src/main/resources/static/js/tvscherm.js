8d1f62ba8745fa302f9e343b914d739991340342
MarcoFacchin <marco.facchin@vdabcampus.be>
2024-06-25 11:35:54 +0200

    JS leest mock met aantal bestellingen

--- src/main/resources/static/js/tvscherm.js
+++ src/main/resources/static/js/tvscherm.js
@@ -1,9 +1,11 @@
 "use strict";
 import {setText, toon} from "./util.js";
-const response = await fetch("bestellingen/aantal");
+//const response = await fetch("bestellingen/aantal");
+const response = await fetch("./aantalbestellingen.json");
+console.log(response);
 if (response.ok) {
-const body = await response.text();
-setText("aantal", body);
+    const body = await response.text();
+    setText("aantal", body);
 } else {
     toon("storing");
 }
