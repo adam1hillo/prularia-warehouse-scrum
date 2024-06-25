53e4d6aad93bf2a2aa9573cc144361089596a514
businan <yorulmazburaksinan@gmail.com>
2024-06-25 15:50:40 +0200

    artikeldetail.html has been revised and js has been added. (with css)

---
+++ src/main/resources/static/js/artikelDetail.js
@@ -0,0 +1,62 @@
+"use strict";
+import {byId, toon, verberg, setText} from "./util.js";
+
+verbergArtikelEnFouten();
+// todo get id from localStorage and pass it to the function
+// const idVanArtikel = JSON.parse(sessionStorage.getItem("idEnNaam"));
+
+toonArtikelDetail();
+
+function verbergArtikelEnFouten() {
+    verberg("artikelTable");
+    verberg("nietGevonden");
+    verberg("storing");
+}
+
+
+async function toonArtikelDetail() {
+    //const response = await fetch(`artikels/${id}`);
+    const response = await fetch(`artikels/6`);
+    if (response.ok) {
+        const artikel = await response.json();
+        toon("artikelTable");
+        setText("artikelNaamVoorHeader", artikel.naam);
+        setText("naam", artikel.naam);
+        setText("id", artikel.artikelId);
+        setText("prijs", `${artikel.prijs} EUR`);
+        setText("gewicht",  `${artikel.gewichtInGram/1000} kg`);
+        setText("voorraad", artikel.voorraad);
+        setText("maximumVoorraad", artikel.maximumVoorraad);
+        setText("maxAantalInMagazijnPlaats", artikel.maxAantalInMagazijnPlaats);
+
+
+        artikel.magazijnPlaatsen.forEach((element) => addRow(element));
+
+    } else {
+        if (response.status === 404) {
+            toon("nietGevonden");
+        } else {
+            toon("storing");
+        }
+    }
+}
+
+function addRow(data) {
+    const tableBody = byId("nestedTableBody");
+    const newRow = document.createElement('tr');
+
+        const newCellVoorRij = document.createElement('td');
+        const newCellVoorRek = document.createElement('td');
+        const newCellVoorAantal = document.createElement('td');
+
+        newCellVoorRij.textContent = data.rij;
+        newCellVoorRek.textContent = data.rek;
+        newCellVoorAantal.textContent = data.aantal;
+        newRow.appendChild(newCellVoorRij);
+        newRow.appendChild(newCellVoorRek);
+        newRow.appendChild(newCellVoorAantal);
+
+
+    tableBody.appendChild(newRow);
+}
+
