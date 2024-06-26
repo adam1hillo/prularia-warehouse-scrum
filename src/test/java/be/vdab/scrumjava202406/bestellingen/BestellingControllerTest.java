169163694e6eb20bc18b72712a41fa176ebc4fe4
Adam Hillo <adam1hillo@gmail.com>
2024-06-26 14:03:34 +0200

    M-TV2.6 directory bestellingen voor test...

--- src/test/java/be/vdab/scrumjava202406/bestellingen/BestellingControllerTest.java
+++ src/test/java/be/vdab/scrumjava202406/bestellingen/BestellingControllerTest.java
@@ -1,4 +1,4 @@
-package be.vdab.scrumjava202406;
+package be.vdab.scrumjava202406.bestellingen;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
