947df76ef3d1059513b91616c3ffcb1c8e5b1642
Gilian Hoskens <gilianhoskens@gmail.com>
2024-06-28 13:23:35 +0200

    Change sql files for tests

---
+++ src/test/resources/cleanUp.sql
@@ -0,0 +1,5 @@
+DELETE FROM magazijnplaatsen WHERE rij='Z' and rek=25;
+DELETE FROM magazijnplaatsen WHERE rij='Z' and rek=26;
+DELETE FROM magazijnplaatsen WHERE rij='Z' and rek=27;
+DELETE FROM magazijnplaatsen WHERE rij='Z' and rek=28;
+DELETE FROM magazijnplaatsen WHERE rij='Z' and rek=29;
