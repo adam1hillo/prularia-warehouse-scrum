583bdfe5e2fae6c88abe2fca97048329cd9d5730
MarcoFacchin <marco.facchin@vdabcampus.be>
2024-07-03 09:13:17 +0200

    ML1.3 artikelId gewijzigd naar eanLastFive in NieuweInkomendeLeveringsLijn DTO

--- src/main/java/be/vdab/scrumjava202406/leveringen/NieuweInkomendeLeveringsLijn.java
+++ src/main/java/be/vdab/scrumjava202406/leveringen/NieuweInkomendeLeveringsLijn.java
@@ -3,5 +3,15 @@ package be.vdab.scrumjava202406.leveringen;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 
-public record NieuweInkomendeLeveringsLijn(@Positive long inkomendeLeveringsId, @Positive long artikelId, @PositiveOrZero int aantalGoedgekeurd, @PositiveOrZero int aantalTeruggestuurd, @Positive long magazijnPlaatsId) {
+public record NieuweInkomendeLeveringsLijn(
+        @Positive
+        long inkomendeLeveringsId,
+        @Positive
+        int eanLastFive,
+        @PositiveOrZero
+        int aantalGoedgekeurd,
+        @PositiveOrZero
+        int aantalTeruggestuurd,
+        @Positive
+        long magazijnPlaatsId) {
 }
