2e897a9c27d013d995b934db967423eda2095968
Gilian Hoskens <gilianhoskens@gmail.com>
2024-07-04 15:24:34 +0200

    BUGFIX | RijRekNieuweAantalTest

--- src/test/java/be/vdab/scrumjava202406/bestellingen/RijRekNieuweAantalTest.java
+++ src/test/java/be/vdab/scrumjava202406/bestellingen/RijRekNieuweAantalTest.java
@@ -41,7 +41,7 @@ public class RijRekNieuweAantalTest {
         RijRekNieuweAantal invalidObject = new RijRekNieuweAantal('A', 0, 3);
         Set<ConstraintViolation<RijRekNieuweAantal>> violations = validator.validate(invalidObject);
         assertFalse(violations.isEmpty());
-        assertTrue(violations.stream().anyMatch(v -> v.getMessage().contains("must be greater than or equal to 1")));
+        assertTrue(violations.stream().anyMatch(v -> v.getMessage().contains("must be greater than or equal to 1") || v.getMessage().contains("moet groter of gelijk aan 1 zijn")));
     }
 
     @Test
@@ -49,6 +49,6 @@ public class RijRekNieuweAantalTest {
         RijRekNieuweAantal invalidObject = new RijRekNieuweAantal('A', 4, -1);
         Set<ConstraintViolation<RijRekNieuweAantal>> violations = validator.validate(invalidObject);
         assertFalse(violations.isEmpty());
-        assertTrue(violations.stream().anyMatch(v -> v.getMessage().contains("must be greater than 0")));
+        assertTrue(violations.stream().anyMatch(v -> v.getMessage().contains("must be greater than or equal to 1") || v.getMessage().contains("moet groter dan 0 zijn")));
     }
 }
